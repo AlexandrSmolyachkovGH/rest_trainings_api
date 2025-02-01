403aabca1de8b0eec9ddbf1b7b66e3c1c9f40012
AlexandrSmolyachkovGH <alexandr.smolyachkov@gmail.com>
2025-02-02 01:11:26 +0300

    refactor: Update the client repo entity tests

--- tests/repositories/clients.py
+++ tests/repositories/clients.py
@@ -1,21 +1,27 @@
 import unittest
-from unittest.mock import AsyncMock, MagicMock
+from unittest.mock import AsyncMock, MagicMock, patch
 from datetime import datetime
 
-from trainings_app.schemas.clients import GetClient, CreateClient, PutClient, PatchClient, ClientFilters, GenderEnum, \
-    ClientStatusEnum
+from trainings_app.schemas.clients import GetClient, GenderEnum, ClientStatusEnum
 from trainings_app.repositories.clients import ClientRepository
 
 
 class TestClientRepository(unittest.IsolatedAsyncioTestCase):
-
-    async def setUp(self):
-        self.mock_session = AsyncMock()
-        self.repo = ClientRepository(self.mock_session)
-
-    @unittest.mock.patch.object(ClientRepository, 'fetchrow_or_404')
-    async def test_get_client(self, mock_fetchrow_or_404):
-        mock_fetchrow_or_404.return_value = {
+    @classmethod
+    def setUpClass(cls):
+        cls.create_input_data = {
+            'user_id': 115,
+            'membership_id': 41,
+            'first_name': 'Test',
+            'last_name': 'Mock',
+            'phone_number': '+14598881420',
+            'gender': GenderEnum.MALE,
+            'date_of_birth': "1982-12-25",
+            'weight_kg': 82,
+            'height_cm': 179,
+            'status': ClientStatusEnum.ACTIVE,
+        }
+        cls.create_db_response = {
             'id': 85,
             'user_id': 115,
             'membership_id': 41,
@@ -28,14 +34,28 @@ class TestClientRepository(unittest.IsolatedAsyncioTestCase):
             'height_cm': 179,
             'status': ClientStatusEnum.ACTIVE,
         }
-        self.assertIsInstance(self.repo, ClientRepository)
-        client = await self.repo.get(85)
 
-        self.assertEqual(client.id, 85)
-        self.assertTrue(
-            all(v > 0 for v in [client.id, client.user_id, client.membership_id, client.weight_kg, client.height_cm])
-        )
-        self.assertTrue(1900 < client.date_of_birth.year <= datetime.now().year)
+    # async def setUp(self):
+    #     """Set up the mock connection"""
+    #     self.mock_pool = MagicMock()
+    #     self.mock_connection = AsyncMock()
+    #     self.mock_pool.acquire.return_value.__aenter__.return_value = self.mock_connection
+    #     self.repo = ClientRepository(self.mock_pool)
+
+    async def asyncSetUp(self):
+        self.repo = ClientRepository(None)
+
+    #@patch.object(ClientRepository, 'fetchrow_or_404', new_callable=AsyncMock)
+    @patch.object(ClientRepository, 'create', new_callable=AsyncMock)
+    async def test_create_client_success(self, mock_create):
+        mock_create.return_value = GetClient(**TestClientRepository.create_db_response)
+        #mock_fetchrow_or_404.return_value = TestClientRepository.create_db_response
+
+        created_client = await self.repo.create(TestClientRepository.create_input_data)
+        self.assertIsInstance(created_client, GetClient)
+        self.assertEqual(created_client.dict()['id'], 85)
+
+
 
 
 if __name__ == '__main__':
